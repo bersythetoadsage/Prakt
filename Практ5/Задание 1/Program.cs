6b02dcd5f41be914e33835c3bab74d6bec9e0fed
bersythetoadsage <107612185+bersythetoadsage@users.noreply.github.com>
2022-07-04 00:58:39 +0300

    Add files via upload

---
+++ Практ5/Задание 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_1
+{
+    class Program
+    {
+        static string path = @"C:\Users\user\source\repos\Ввод-вывод\Задание 1\Для задания 1\";
+        static void Main(string[] args)
+        {
+            DirectoryInfo directoryinfo = new DirectoryInfo("папка");
+
+            for (int i = 1; i <= 100; i++)
+                if(!directoryinfo.Exists)
+                    Directory.CreateDirectory(path+i);
+
+            Console.WriteLine("Enter для удаления");
+            Console.ReadLine();
+
+            for (int i = 1; i <= 100; i++)
+                if (!directoryinfo.Exists)
+                    Directory.Delete(path + i);
+        }
+    }
+}
---
+++ Практ5/Задание 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_2
+{
+    class Program
+    {
+        static string path = @"C:\Users\user\source\repos\Ввод-вывод\задание2.txt";
+        static void Main(string[] args)
+        {
+            using (FileStream fs = File.Create(path)) { }
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                        sw.WriteLine("текст");
+                }
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                    Console.WriteLine(sr.ReadLine());
+            }
+            Console.ReadLine();
+        }
+    }
+}
